8aa7d2817cf0710935f7dfe422d848430ccdd186
Vadim Vasiatullin <vasiatyllin@yandex.ru>
2024-01-20 17:30:15 +0300

    Initial Commit

---
+++ VadimCard/VadimCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VadimCardApp.swift
+//  VadimCard
+//
+//  Created by vadim vasiatullin on 20.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct VadimCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
